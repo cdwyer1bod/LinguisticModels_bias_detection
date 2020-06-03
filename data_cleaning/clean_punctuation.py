d80228c404b63c0b81e5855293e680eb4c716872
Chris Dwyer <chrisdwyer@Chriss-MacBook-Pro-2.local>
2020-06-03 13:12:02 +1000

    final datasets

--- data_cleaning/clean_punctuation.py
+++ data_cleaning/clean_punctuation.py
@@ -1,17 +1,25 @@
 import numpy as np
 import csv
-data = ['Before Edit','After Edit','Before Edit Sentence','After Edit Sentence']
-newfile = open('train-sents-final.csv','a+',newline='')
+data = []
+newfile = open('dev-sents-final1.csv','a+',newline='')
 
-with open('./5gram-edits-train-sents-clean.csv', newline='') as csvfile:
+def containsAny(string):
+    chars = ['{', '}', '[', ']','|','*']
+    for c in chars:
+        if c in string: return 1
+    return 0
+
+with open('./5gram-edits-dev-sents-clean.csv', newline='') as csvfile:
     reader = csv.reader(csvfile)
     for row in reader:
-        before = row[6].replace('.','').replace(',','').lower()
-        after = row[7].replace('.','').replace(',','').lower()
-        sents_before = row[8].replace('.','').replace(',','').lower()
-        sents_after = row[9].replace('.','').replace(',','').lower()
-        before = before.replace('.','').replace(',','').lower()
-        data.append([before,after,sents_before,sents_after])
+        before = row[6].replace('.','').replace(',','').replace('"','').lower()
+        after = row[7].replace('.','').replace(',','').replace('"','').lower()
+        sents_before = row[8].replace('.','').replace(',','').replace('"','').lower()
+        sents_after = row[9].replace('.','').replace(',','').replace('"','').lower()
+        if containsAny(sents_before) == 0:
+            if sents_before != sents_after:
+                data.append([before,after,sents_before,sents_after])
+
 
 with newfile:
     write = csv.writer(newfile)
