adf0f75f21f525f917577a297e8fe94a8487e13a
Darcy Osullivan <darcyosullivan@Darcys-MacBook-Pro.local>
2020-06-03 13:02:09 +1000

    Added functions for word and POS features

--- vectorize.py
+++ vectorize.py
@@ -13,7 +13,7 @@ def getPOStag(x):
         tokenized_text = word_tokenize(text)
         return pos_tag(tokenized_text)
     else:
-        raise ValueError("getNERtag requires a string for the first input")
+        raise ValueError("getPOStag requires a string for the first input")
 
 def getPOSdata(taggedsent, index):
     # Takes a tagged sent and the index of the word to be vectorized
@@ -36,9 +36,6 @@ def get_bais_index(x):
         #TODO: remove punctuatuion from column 7 so that we dont have to drop these cases
         return None
 
-def get_tag_at_pos(x, index, n):
-    return x['tagged_sent'][index+n][1]
-
 def get_bais_tag(x, n):
     if not np.isnan(x['bais_index']):
         if (x['bais_index'] + n > 0) and (x['bais_index'] + n < len(x['tagged_sent'])):
@@ -46,16 +43,34 @@ def get_bais_tag(x, n):
     else:
         return None
 
-def get_pos_freqs(filename):
+
+def get_freqs(filename):
     data = pd.read_csv(filename, ",")
     data['bais_index'] = data.apply(get_bais_index, axis = 1)
     data['tagged_sent'] = data.apply(getPOStag, axis = 1)
-    freq_dists = {}
+    pos_freq_dists = {}
     for i in range(-2, 3):
         data['bais_tag'] = data.apply(get_bais_tag, n = i, axis = 1)
-        freq_dist = data['bais_tag'].value_counts()/len(data)
-        freq_dists[i] = freq_dist/freq_dist[0]
-    return freq_dists
+        pos_freq_dist = data['bais_tag'].value_counts()/len(data)
+        pos_freq_dists[i] = pos_freq_dist/pos_freq_dist[0]
+    word_freq_dist = data['7'].value_counts()/len(data)
+    word_freq_dist = word_freq_dist/word_freq_dist[0]
+    return pos_freq_dists, word_freq_dist.to_dict()
 
-print(get_pos_freqs("5gram-edits-test-sents-clean.csv"))
+def vectorize_word(w, POS_FREQ_DIST):
+    if w in POS_FREQ_DIST.keys():
+        return POS_FREQ_DIST[w]
+    else:
+        return 0
+
+def vectorize_pos_n(pos, n, POS_FREQ_DIST):
+    if pos in POS_FREQ_DIST[n].keys():
+        return POS_FREQ_DIST[n][pos]
+    else:
+        return 0
+
+#TODO: save these FREQ... somewhere to save doing the comp every time
+#      potentially when running big function include option to redo freq lists
+POS_FREQ_DIST, WORD_FREQ_DIST = get_freqs("5gram-edits-test-sents-clean.csv")
+print(type(WORD_FREQ_DIST))
 
