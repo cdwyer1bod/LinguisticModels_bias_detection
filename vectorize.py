d797fc98f8beebe40a5a7b3d56f90f3209fdf0fc
Darcy Osullivan <darcyosullivan@Darcys-MacBook-Pro.local>
2020-06-04 15:26:29 +1000

    Built wrapper to vectorize data in tester notebook

--- vectorize.py
+++ vectorize.py
@@ -6,18 +6,18 @@ from nltk.tokenize import word_tokenize
 from nltk import pos_tag
 
 text = 'While in France, Christine Lagarde discussed short-term stimulus efforts in a recent interview with the Wall Street Journal.'
-columns = ["bias_word", "neutral_words", "bias_sent", "neutral_sent"]
+columns = ["Bias Inducing Word", "De-Biased Word", "Sentence Pre-Edit", "Sentence Post-Edit"]
 
-def getPOStag(x):
+def get_pos_tag(x):
     #takes row and extracts the raw text and returns list of POS tagged tupples
-    text = x['bias_sent']
+    text = x['Sentence Pre-Edit']
     if type(text) is str:
         tokenized_text = word_tokenize(text)
         return pos_tag(tokenized_text)
     else:
-        raise ValueError("getPOStag requires a string for the first input")
+        raise ValueError("get_pos_tag requires a string for the first input")
 
-def getPOSdata(taggedsent, index):
+def get_POS_data(taggedsent, index):
     # Takes a tagged sent and the index of the word to be vectorized
     # If POS +/- N is out of range returns None
     POSdata = []
@@ -31,9 +31,9 @@ def getPOSdata(taggedsent, index):
 
 def get_bias_index(x):
     #Ineffecient to tokenize twice, write these functions in parallel
-    tokenized_text = word_tokenize(x['bias_sent'])
+    tokenized_text = word_tokenize(x['Sentence Pre-Edit'])
     try:
-        return int(tokenized_text.index(x['bias_word']))
+        return int(tokenized_text.index(x['Bias Inducing Word']))
     except:
         #TODO: remove punctuatuion from column 7 so that we dont have to drop these cases
         return None
@@ -53,7 +53,7 @@ def get_corpus_freqs(df):
     for row in df['tagged_sent'].values:
         for pair in row:
             tags_freq[pair[1]]+=1.0
-    for row in df['bias_sent'].values:
+    for row in df['Sentence Pre-Edit'].values:
         for word in row.split(' '):
             words_freq[word]+=1.0
     return tags_freq, words_freq
@@ -65,7 +65,7 @@ def get_freqs(filename):
     data = pd.read_csv(filename, ",")
     data.columns = columns
     data['bias_index'] = data.apply(get_bias_index, axis = 1)
-    data['tagged_sent'] = data.apply(getPOStag, axis = 1)
+    data['tagged_sent'] = data.apply(get_pos_tag, axis = 1)
     tags_freq, words_freq = get_corpus_freqs(data)
     pos_freq_dists = {}
     for i in range(-2, 3):
@@ -74,7 +74,7 @@ def get_freqs(filename):
         for index in pos_freq_dist.index:
             pos_freq_dist[index] = pos_freq_dist[index]/tags_freq[index]
         pos_freq_dists[i] = pos_freq_dist/max(pos_freq_dist)
-    word_freq_dist = data['bias_word'].value_counts().astype(float)
+    word_freq_dist = data['Bias Inducing Word'].value_counts().astype(float)
     for index in word_freq_dist.index:
         try:
             word_freq_dist[index] = float(float(word_freq_dist[index])/float(words_freq[index]))
@@ -83,9 +83,9 @@ def get_freqs(filename):
     word_freq_dist = word_freq_dist/max(word_freq_dist)
     return pos_freq_dists, word_freq_dist.to_dict()
 
-def vectorize_word(w, POS_FREQ_DIST):
-    if w in POS_FREQ_DIST.keys():
-        return POS_FREQ_DIST[w]
+def vectorize_word(w, WORD_FREQ_DIST):
+    if w in WORD_FREQ_DIST.keys():
+        return WORD_FREQ_DIST[w]
     else:
         return 0
 
@@ -100,5 +100,5 @@ def vectorize_pos_n(pos, n, POS_FREQ_DIST):
 
 
 POS_FREQ_DIST, WORD_FREQ_DIST = get_freqs("./final_datasets/train-sents-final1.csv")
-print(WORD_FREQ_DIST)
-print(POS_FREQ_DIST)
+#print(WORD_FREQ_DIST)
+#print(POS_FREQ_DIST)
